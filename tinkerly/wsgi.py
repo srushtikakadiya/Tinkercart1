b6e13b18b5658d7dcca5d01bab8f9e6fe9a7e202
srushti kakadiya <srushtikakadiya2701@gmail.com>
2021-06-01 11:08:14 +0530

    new change

--- tinkerly/wsgi.py
+++ tinkerly/wsgi.py
@@ -1,5 +1,5 @@
 """
-WSGI config for shoppinglyx project.
+WSGI config for tinkercart project.
 
 It exposes the WSGI callable as a module-level variable named ``application``.
 
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'shoppinglyx.settings')
+os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'tinkercart.settings')
 
 application = get_wsgi_application()
